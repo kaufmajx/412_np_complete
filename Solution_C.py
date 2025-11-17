4aba80e294969268ac8ff3210240765a9ea68d5c
Jelal Kaufman <kaufmajx@dukes.jmu.edu>
2025-11-17 09:26:45 -0500

    Initial Push

---
+++ Solution_C.py
@@ -0,0 +1,19 @@
+"""
+Name: Morgan Wagner
+Honor Code and Acknowledgments:
+This work complies with the JMU Honor Code.
+Comments here on your code and submission.
+"""
+
+def main():
+    
+    in_list = list(map(int, input().split()))
+    search_item = int(input())
+
+    print(in_list)
+    print(search_item)
+
+
+
+if __name__ == "__main__":
+    main()
